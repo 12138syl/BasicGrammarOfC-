253b7b3d99c924bd32b5260fee73d09f29de867a
vscode-syl <2072188582@qq.com>
2024-07-12 08:29:01 +0800

    第一次修改

---
+++ 千锋C++/Basic_Grammar/00_hello_world/00_hello_world_in_oop.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+class HelloWorld
+{
+    public:
+        void printHelloWorld()
+        {
+            std::cout << "Hello World in oop!\n";
+        }
+};
+int main()
+{
+    HelloWorld hw;
+    hw.printHelloWorld();
+    return 0;
+
+    
+}
