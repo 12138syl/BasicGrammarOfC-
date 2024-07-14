f7d3884e194d0c6fa9edb7a26f2ef4f1acc07aed
vscode-syl <2072188582@qq.com>
2024-07-14 15:07:44 +0800

    static

---
+++ Basic_Grammar/01_basic_things/11_static.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// 定义一个函数，其中包含局部静态变量
+void func() {
+    static int count = 0; // 这是一个局部静态变量，其作用域限制在func()函数内，
+    // 但是其生存周期贯穿整个程序运行过程。
+    count++;
+    std::cout << "This function has been called " << count << " times.\n";
+}
+
+// 主函数
+int main() {
+    func(); // 第一次调用func()
+    func(); // 第二次调用func()
+    // 注意，由于count是静态的，它的值将在两次调用之间保持不变。
+    return 0;
+}
